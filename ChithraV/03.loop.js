989ecb612a701142851470e36fbf5b1eff714115
chithraviswan <chithraviswan7@gmail.com>
2025-01-03 16:50:34 +0530

    Create 03.loop.js

---
+++ ChithraV/03.loop.js
@@ -0,0 +1,11 @@
+function printNumbers(numbers) {
+    for(let i=1;i<=5;i++)
+        console.log(i)
+  }
+  printNumbers([1, 2, 3, 4, 5]); 
+  // Expected Output: 
+  // 1
+  // 2
+  // 3
+  // 4
+  // 5
